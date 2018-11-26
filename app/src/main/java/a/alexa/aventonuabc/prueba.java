6109a65560396119f79f0a40d43831ec0112a9ab
Carlos Saavedra <36249113+mastercas1@users.noreply.github.com>
2018-11-25 22:18:33 -0800

    comm

--- app/src/main/java/a/alexa/aventonuabc/prueba.java
+++ app/src/main/java/a/alexa/aventonuabc/prueba.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class prueba extends AppCompatActivity {
-
+// clase de prueba
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
